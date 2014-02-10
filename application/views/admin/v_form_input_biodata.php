8cd90457f7e1e607b6c9593faafed85be448adb0
ansharabdullah <dull92@gmail.com>
2014-02-10 16:44:13 +0700

    second

---
+++ application/views/admin/v_form_input_biodata.php
@@ -0,0 +1,59 @@
+
+
+<!-- biodata -->
+			
+<div id="dashboard-left" class="span8" style="width:95.5%;">
+        <div class="widgetbox">
+                <h4 class="widgettitle"><span class="icon-book icon-white"></span>Biodata<a class="close">&times;</a> <a class="minimize">&#8211;</a></h4>
+                <div class="widgetcontent" style="padding:10px 50px 30px 50px">
+                        <div style="width:100%">
+                            <br />
+                            <form action="" method="post">
+                                <table style="font-size:14px; margin-left:200px; margin-top:-160px;">
+                                <tr>
+                                        <td>NIP (Nomor Induk Pegawai)</td>
+                                        <td>:</td>
+                                        <td><input type="text"></td>
+                                </tr>
+                                <tr height="8"></tr>
+                                <tr>
+                                        <td>NIP Lama</td>
+                                        <td>:</td>
+                                        <td><input type="text"></td>
+                                </tr>
+                                <tr height="8"></tr>
+                                <tr>
+                                        <td>Nama Lengkap</td>
+                                        <td>:</td>
+                                        <td></td>
+                                </tr>
+                                 <tr height="8"></tr>
+                                <tr>
+                                        <td>Gelar Depan</td>
+                                        <td>:</td>
+                                        <td></td>
+                                </tr>
+                                 <tr height="8"></tr>
+                                <tr>
+                                        <td>Gelar Belakang</td>
+                                        <td>:</td>
+                                        <td></td>
+                                </tr>
+                                <tr>
+                                        <td>Tempat Lahir</td>
+                                        <td>:</td>
+                                        <td></td>
+                                </tr>
+                                <tr height="8"></tr>
+                                <tr>
+                                        <td>Tanggal Lahir</td>
+                                        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : &nbsp;</td>
+                                        <td></td>
+                                </tr>
+                       
+                        </table>
+                            </form>
+                        </div>
+                </div>
+        </div>
+</div>
