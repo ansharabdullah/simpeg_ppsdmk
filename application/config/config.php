39be3dbeb4adc78d5a446819fc445d2f7ab3a4ce
Indana Zulfa <izulfa28@gmail.com>
2014-02-10 16:53:31 +0700

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/tobrut/';
+$config['base_url']	= 'http://localhost/simpeg_ppsdmk/';
 
 /*
 |--------------------------------------------------------------------------
