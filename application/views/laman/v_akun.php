01f852ffedb50cdd36cfe0f9d2ecc9b1e57da9e5
ansharabdullah <dull92@gmail.com>
2014-02-11 16:37:21 +0700

    second

---
+++ application/views/laman/v_akun.php
@@ -0,0 +1,58 @@
+
+<div class="regpanel">
+    <div class="regpanelinner">
+        <div class="pageheader">
+            <div class="pageicon"><span class="iconfa-hand-down"></span></div>
+            <div class="pagetitle">
+                <h5>Your Information</h5>
+                <h1>Sign Up</h1>
+            </div>
+        </div>
+        <div class="regcontent">
+
+
+
+            <h4 class="widgettitle" style="width:50%; margin-left:25%;"><span class="icon-list-alt icon-white"></span>Pengaturan Akun</h4>
+            <div class="widgetcontent" style="width:50%; margin-left:25%;">
+                <form action="" method="post" class="stdform">
+
+                    <h3 class="subtitle">Login Information</h3>
+                    <p><input type="text" name="username" class="input-block-level" placeholder="Username" /></p>
+                    <p><input type="password" name="password" class="input-block-level" placeholder="Password" /></p>
+
+                    <h3 class="subtitle">User Information</h3>
+                    <p><input type="text" name="firstname" class="input-block-level" placeholder="Firstname" /></p>
+                    <p><input type="text" name="lastname" class="input-block-level" placeholder="Lastname" /></p>
+
+                    <h3 class="subtitle">Birthday</h3>
+                    <p class="bday">
+                        <select name="month">
+                            <option>Month</option>
+                            <option value="January">January</option>
+                            <option value="February">February</option>
+                        </select>
+                        <select name="day">
+                            <option>Day</option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                        </select>
+                        <select name="year">
+                            <option>Year</option>
+                            <option value="1990">1990</option>
+                            <option value="1991">1991</option>
+                        </select>
+                    </p>
+
+                    <h3 class="subtitle">Gender</h3>
+                    <p>
+                        <input type="radio" /> Male &nbsp;&nbsp;
+                        <input type="radio" /> Female
+                    </p>
+                    <p><button class="btn btn-primary">Register</button></p>
+
+                </form>
+
+            </div><!--regcontent-->
+        </div><!--regpanelinner-->
+    </div><!--regpanel-->
+</div>
